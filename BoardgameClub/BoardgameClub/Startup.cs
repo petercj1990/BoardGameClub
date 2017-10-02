93f50edcf6d72215ca780a3ce4639469920fe7e8
Peter Jensen <petetjensen@gmail.com>
2017-10-02 16:17:14 -0600

    Initial setup of BoardGame Website
    .net MVC app with mySQL server

---
+++ BoardgameClub/BoardgameClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoardgameClub.Startup))]
+namespace BoardgameClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

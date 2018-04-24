bfce3b91bafab3538351d58aa3601095c9628ceb
Peter Jensen <pjensen@healthequity.com>
2018-04-24 16:31:32 -0600

    stuff for richard

---
+++ BoardGameClubEntities/Participant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameClubEntities
+{
+    public class Participant
+    {
+        public Participant()
+        {
+        }
+        [Key]
+        public int Id { get; set; }
+        public int Player_Id { get; set; }
+        public int PlaySession_Id { get; set; }
+        public bool? Winner { get; set; }
+        public int? Team { get; set; }
+    }
+}
